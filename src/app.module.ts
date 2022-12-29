5e66fe4865806bca13d827d8765fd2149d5d4571
andrejaques <andrejaques.b@gmail.com>
2022-12-28 22:46:05 -0300

    add first  models, imports, controllers and providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MailModule } from './modules/mail/email.module';
 import { MailService } from './modules/mail/mail.service';
 import { SMTPMailService } from './modules/mail/smtp-mail.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController, MailService],
-  providers: [AppService, SMTPMailService],
+  imports: [MailModule],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: MailService,
+      useClass: SMTPMailService,
+    },
+  ],
 })
 export class AppModule {}
--- src/modules/mail/smtp-mail.service.ts
+++ src/modules/mail/smtp-mail.service.ts
@@ -1,5 +1,7 @@
+import { Injectable } from '@nestjs/common';
 import { MailService } from './mail.service';
 
+@Injectable()
 export class SMTPMailService implements MailService {
   sendEmail(): string {
     return 'SMTP Mail';
