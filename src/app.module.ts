c8b76296dadedc42161ef17b09b4e3d2d8cf810a
andrejaques <andrejaques.b@gmail.com>
2022-12-31 19:12:47 -0300

    add prisma with postrres

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { MailModule } from './modules/mail/email.module';
 import { MailService } from './modules/mail/mail.service';
 import { SMTPMailService } from './modules/mail/smtp-mail.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [MailModule],
   controllers: [AppController],
   providers: [
     AppService,
+    PrismaService,
     {
       provide: MailService,
       useClass: SMTPMailService,
